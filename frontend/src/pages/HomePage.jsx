78acc91701d90b5b057936d09b9d3d1409836b15
HAMMER17 <plutalov70@gmail.com>
2025-06-10 22:08:57 +0300

    add bg-black

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import HomeScreen from '../home/HomeScreen'
 import AuthScreen from '../home/AuthScreen'
 import { useAuthStore } from '../store/authUser'
@@ -6,7 +6,7 @@ import { useAuthStore } from '../store/authUser'
 const HomePage = () => {
   const { user } = useAuthStore()
   return (
-    <div >
+    <div className='bg-black'>
       {user ? <HomeScreen /> : <AuthScreen />}
     </div>
   )
